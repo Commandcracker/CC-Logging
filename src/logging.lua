4b361ab76638fad299aba9b617ab37902237a563
Commandcracker <lukas.Fricke.dev@gmail.com>
2022-06-19 11:44:30 +0200

    added usage to Documentation

--- src/logging.lua
+++ src/logging.lua
@@ -1,14 +1,37 @@
---- An logging library for Computer Craft
--- @module logging
+--[[- An logging library for Computer Craft
+    @module logging
+    @usage Example:
 
---[[
-    TODO: RednetHandler
-    TODO: RednetReciever
-    TODO: multishell support
+        local logging = require("logging")
+        logging:info("Message logged with logging library")
+
+    Example with logger:
+
+        local logging = require("logging")
+        local logger  = logging.Logger.new(shell.getRunningProgram())
+
+        logger:info("Message logged with logging library using a logger")
 ]]
 
---- Holds information of an log level.
--- @type Level
+--[[
+    TODO: improve RednetHandler
+    TODO: create RednetReciever
+    TODO: add multishell support
+    TODO: rework all Handler's
+    TODO: rename classses to be more descriptive
+    TODO: overhaule Log levels
+]]
+
+--[[- Holds information of an log level.
+    @type Level
+    @usage Example:
+
+        local logging = require("logging")
+        local logger  = logging.Logger.new(shell.getRunningProgram())
+        local level   = logging.Level.new("My Logger", nil, colours.orange, colours.blue)
+
+        logger:log(level, "This message will be logged with your custom level")
+]]
 local Level = {}
 
 --- Create's a new Level instance.
@@ -36,26 +59,37 @@ local Record = {}
 -- @tparam number name The name of the used logger
 -- @treturn Record instance
 function Record.new(level, message, name)
-    local instance     = setmetatable({}, { __index = Record })
-    instance.level     = level
-    instance.message   = message
-    instance.time      = os.time(os.date("*t"))
-    instance.localtime = os.time(os.date("!*t"))
-    instance.name      = name
-    instance.day       = os.day()
+    local instance         = setmetatable({}, { __index = Record })
+    instance.level         = level
+    instance.message       = message
+    instance.time          = os.time(os.date("*t"))
+    instance.localtime     = os.time(os.date("!*t"))
+    instance.name          = name
+    instance.day           = os.day()
+    instance.computerid    = os.getComputerID()
+    instance.computerlabel = os.getComputerLabel()
     return instance
 end
 
 --[[- Formats a Record instance to a string.
     - fmt placeholders: 
-        - `%(name)`        Name of the logger
-        - `%(levelname)`   Name of the Log level
-        - `%(message)`     The Message to log
-        - `%(time)`        The real live time formatted by datefmt
-        - `%(localtime)`   The minecraft time formatted by datefmt
-        - `%(day)`         The minecraft day
+        - `%(name)`          Name of the logger
+        - `%(levelname)`     Name of the Log level
+        - `%(message)`       The Message to log
+        - `%(time)`          The real live time formatted by datefmt
+        - `%(localtime)`     The minecraft time formatted by datefmt
+        - `%(day)`           The minecraft day ([os.day()](https://tweaked.cc/module/os.html#v:day))
+        - `%(computerid)`    The ID of the computer ([os.getComputerID()](https://tweaked.cc/module/os.html#v:getComputerID))
+        - `%(computerlabel)` The label of the computer ([os.getComputerLabel()](https://tweaked.cc/module/os.html#v:getComputerLabel))
     - datefmt works like the format in [os.date()](https://tweaked.cc/module/os.html#v:date)
     @type Formatter
+    @usage Example:
+
+        local logging   = require("logging")
+        local formatter = logging.Formatter.new("[%(time) %(name) %(levelname)] %(message)", "%Y-%m-%d %H:%M:%S")
+        local logger    = logging.Logger.new(shell.getRunningProgram(), nil, formatter)
+
+        logger:info("This message will be logged with your custom formatter")
 ]]
 local Formatter = {}
 
@@ -81,11 +115,24 @@ function Formatter:format(record)
     temp = temp:gsub("%%%(time%)", os.date(self.datefmt, record.time))
     temp = temp:gsub("%%%(localtime%)", os.date(self.datefmt, record.localtime))
     temp = temp:gsub("%%%(day%)", record.day)
+    temp = temp:gsub("%%%(computerid%)", record.computerid)
+    temp = temp:gsub("%%%(computerlabel%)", record.computerlabel)
     return temp
 end
 
---- broadcast the formatted record message with rednet.
--- @type RednetHandler
+--[[- broadcast the formatted record message with rednet.
+    @type RednetHandler
+    @usage Example:
+
+        local logging = require("logging")
+        local logger  = logging.Logger.new(shell.getRunningProgram())
+
+        rednet.open("right") -- replace "right" with the side your modem is connected to
+        local rednetHandler = logging.RednetHandler.new(logger.formatter)
+        logger:addHandler(rednetHandler)
+
+        logger:info("This message will be broadcasted over rednet")
+]]
 local RednetHandler = {}
 
 --- Create's a new RednetHandler instance.
@@ -103,8 +150,22 @@ function RednetHandler:handle(record)
     rednet.broadcast(self.formatter:format(record))
 end
 
---- Will send the formatted record message to a websocket.
--- @type WebsocketHandler
+--[[- Will send the formatted record message to a websocket.
+    @type WebsocketHandler
+    @usage Example:
+
+        local logging = require("logging")
+        local logger  = logging.Logger.new(shell.getRunningProgram())
+
+        local websocket = http.websocket("ws://localhost:8080/")
+        -- replace "localhost:8080" with the address of your websocket server
+        local websocketHandler = logging.WebsocketHandler.new(logger.formatter, websocket)
+        logger:addHandler(websocketHandler)
+
+        logger:info("This message will be sent to the websocket")
+
+        websocket.close() -- don't forget to close the websocket after your script has finished
+]]
 local WebsocketHandler = {}
 
 --- Create's a new WebsocketHandler instance.
@@ -124,8 +185,22 @@ function WebsocketHandler:handle(record)
     self.websocket.send(self.formatter:format(record))
 end
 
---- Will send the record with the used formatter to a websocket.
--- @type RawWebsocketHandler
+--[[- Will send the record with the used formatter to a websocket.
+    @type RawWebsocketHandler
+    @usage Example:
+
+        local logging = require("logging")
+        local logger  = logging.Logger.new(shell.getRunningProgram())
+
+        local websocket = http.websocket("ws://localhost:8080/")
+        -- replace "localhost:8080" with the address of your websocket server
+        local rawWebsocketHandler = logging.RawWebsocketHandler.new(logger.formatter, websocket)
+        logger:addHandler(rawWebsocketHandler)
+
+        logger:info("This message will be sent to the websocket in raw format")
+
+        websocket.close() -- don't forget to close the websocket after your script has finished
+]]
 local RawWebsocketHandler = {}
 
 --- Create's a new RawWebsocketHandler instance.
@@ -284,8 +359,22 @@ local function convertBackgroundColor(color)
     return code
 end
 
---- Will send the formatted record message with ansi escape codes (for the color) to a websocket.
--- @type ColordWebsocketHandler
+--[[- Will send the formatted record message with [ansi escape codes (for the color)](https://en.wikipedia.org/wiki/ANSI_escape_code#Colors) to a websocket.
+    @type ColordWebsocketHandler
+    @usage Example:
+
+        local logging = require("logging")
+        local logger  = logging.Logger.new(shell.getRunningProgram())
+
+        local websocket = http.websocket("ws://localhost:8080/")
+        -- replace "localhost:8080" with the address of your websocket server
+        local colordWebsocketHandler = logging.ColordWebsocketHandler.new(logger.formatter, websocket)
+        logger:addHandler(colordWebsocketHandler)
+
+        logger:info("This message will be sent to the websocket with its color in ansi escape codes")
+
+        websocket.close() -- don't forget to close the websocket after your script has finished
+]]
 local ColordWebsocketHandler = {}
 
 --- Create's a new ColordWebsocketHandler instance.
@@ -320,8 +409,22 @@ function ColordWebsocketHandler:handle(record)
     )
 end
 
---- An FileHandler that writes to a file.
--- @type FileHandler
+--[[- An FileHandler that writes to a file.
+    @type FileHandler
+    @usage Example:
+
+        local logging = require("logging")
+        local logger  = logging.Logger.new(shell.getRunningProgram())
+
+        local file = fs.open("example.log", "a")
+        -- replace "example.log" with the path of your log file
+        local fileHandler = logging.FileHandler.new(logger.formatter, file)
+        logger:addHandler(fileHandler)
+
+        logger:info("This message will be sent to the file")
+
+        file.close() -- don't forget to close the file after your script has finished
+]]
 local FileHandler = {}
 
 --- Create's a new FileHandler instance.
@@ -341,8 +444,20 @@ function FileHandler:handle(record)
     self.file.writeLine(self.formatter:format(record))
 end
 
---- An Handler that writes to the terminal.
--- @type TerminalHandler
+--[[- An Handler that writes to the terminal.
+    @type TerminalHandler
+    @usage Example:
+
+        local logging = require("logging")
+        local logger  = logging.Logger.new(shell.getRunningProgram())
+
+        -- the logger will have a TerminalHandler by default
+        -- but if you want to add another one you can do it like this:
+            -- local terminalHandler = logging.TerminalHandler.new(logger.formatter)
+            -- logger:addHandler(terminalHandler)
+
+        logger:info("This message will be sent to the terminal")
+]]
 local TerminalHandler = {}
 
 --- Create's a new TerminalHandler instance.
@@ -371,19 +486,27 @@ function TerminalHandler:handle(record)
     write("\n")
 end
 
---- Holds all functions related to logging.
--- @type Logger
+--[[- Holds all functions related to logging.
+    @type Logger
+    @usage Example:
+
+        local logging = require("logging")
+        local logger  = logging.Logger.new(shell.getRunningProgram())
+
+        logger:info("This message will be logged")
+]]
 local Logger = {}
 
 --- Create's a new Logger instance.
 -- @tparam string name The logger name.
 -- @tparam number level The level weight.
+-- @tparam Formatter formatter The formatter to use.
 -- @treturn Logger instance
-function Logger.new(name, level)
+function Logger.new(name, level, formatter)
     local instance     = setmetatable({}, { __index = Logger })
     instance.name      = name
     instance.level     = level or 0
-    instance.formatter = Formatter.new()
+    instance.formatter = formatter or Formatter.new()
     instance.levels    = {
         ["DEBUG"]    = Level.new("DEBUG", 10, colours.cyan),
         ["INFO"]     = Level.new("INFO", 20, colours.green),
